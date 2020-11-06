c7de7382a66f20ff93e3fd3723afabec6232a6df
justin-moretto <JustinMorettoMTL@gmail.com>
2020-11-06 16:22:14 +0000

    refactored and added mocha test; encountering Timeout error

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
 const request = require('request');
-const breed = process.argv.slice(2,3);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    throw error;
-  } else if (!breed) {
-    return [];
-  } else if (body === '[]') {
-    console.log('Breed not found.');
-  } else {
-    const catData = JSON.parse(body);
-    console.log(catData[0].description);
-  }
-});
+const fetchBreedDescription = (breed, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      callback(error, null); //I'm a bit confused about this line. Have to study callbacks more
+    } else if (!breed) {
+      return [];
+    } else if (body === '[]') {
+      callback(`${breed} not found.`, null);
+    } else {
+      const catData = JSON.parse(body);
+      console.log(catData[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
